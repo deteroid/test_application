27c01385af7a23f8cadb97f32a0fdb6e795a1a16
deteroid <omkarphd@gmail.com>
2021-03-26 16:32:07 +0530

    updated for on change and value

--- src/App.js
+++ src/App.js
@@ -23,14 +23,37 @@ class App extends Component {
     })
   }
 
+  switchHandlerInput = (event) => {
+    this.setState({
+      person: [
+        { name: event.target.value, age: 24 },
+        { name: "Abhishek", age: 20 },
+        { name: "Akash", age: 25 },
+
+      ]
+    })
+  }
+
+
+
   render() {
     return (
       <div className="App">
         <h1>This is H1</h1>
         <button onClick={this.switchHandler} >Switch name</button>
-        <Person name={this.state.person[0].name} age={this.state.person[0].age} />
-        <Person name={this.state.person[1].name} age={this.state.person[1].age} />
-        <Person name={this.state.person[2].name} age={this.state.person[2].age} />
+        <Person
+          name={this.state.person[0].name}
+          age={this.state.person[0].age} 
+          click={this.switchHandlerInput}
+          />
+        <Person
+          name={this.state.person[1].name}
+          age={this.state.person[1].age} 
+          />
+        <Person
+          name={this.state.person[2].name}
+          age={this.state.person[2].age} 
+          />
 
       </div>
     )
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 const person = (props) => {
     return (
-        <h1>I am a {props.name} and my age is {props.age}</h1>
+        <div>
+            <h1>I am a {props.name} and my age is {props.age}</h1>
+            <input type="text" onChange={props.click} value={props.name}></input>
+        </div>
+
     )
 }
 
